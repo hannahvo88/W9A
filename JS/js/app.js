ab427614b55ac6a5d39d73e116a1a7fd0982bbe1
Hannah <hannah88.vo@gmail.com>
2021-07-07 18:29:26 -0600

    W9A

---
+++ JS/js/app.js
@@ -0,0 +1,13 @@
+var age = 17;
+var is_subcribed = true;
+is_subcribed = false;
+
+if (age < 18 && !is_subcribed) {
+    console.log ("The user is younger than 18 and is not subscribed")
+}else if (age >=18 && !is_subcribed){
+    console.log ("The user is older than 18 and is not subscribed")
+}else if (age < 18 && is_subcribed){
+    console.log ("The user is younger than 18 and is subscribed")
+}else{
+    console.log ("The user is older than 18 and is subscribed")
+}
